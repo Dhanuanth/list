97e7a30db85968f21fd0f81edc6c4b0d24499770
Dhanuanth <66349240+Dhanuanth@users.noreply.github.com>
2020-06-12 10:06:08 +0530

    Update 2 nd ass.py

--- 2 nd ass.py
+++ 2 nd ass.py
@@ -1,14 +1,23 @@
+
 list_1=[]
-n=int(input("enter number of elements in the list :"))
-print('enter the elements')
-for i in range(n):
 
-    x=int(input('index %d :'%(i)))
-    list_1.append(x)
+list_1=[int(ele) for ele in input('enter the elements in list1(seperated by spaces): ').split() ] # enter the elements seperated by spaces
+i=0
+while(i<len(list_1)):
+    if list_1[i]<0:
+        list_1.remove(list_1[i])
+    else:
+        i+=1
+print(*list_1,sep=',')
+#############
+list_2=[]
 
-print('your list is ',list_1)
-print('negative numbers are')
-for i in list_1:
-    if i<0:
-        print(i )
+list_2=[int(ele) for ele in input('enter the elements in list2(seperated by spaces): ').split() ]# enter the elements seperated by spaces
+i=0
+while(i<len(list_2)):
+    if list_2[i]<0:
+        list_2.remove(list_2[i])
+    else:
+        i+=1
+print(list_2)
 
